f5e8ca73a9c97aa21371101d6f66c72c021c6c60
EagleVee <vu.nguyen8@mservice.com.vn>
2024-06-25 17:58:54 +0700

    attempt to use vercel edge

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,7 +1,26 @@
 import * as trpcNext from '@trpc/server/adapters/next';
 import { appRouter } from '@/server/routers/_app';
+import { NextRequest } from 'next/server';
+import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 
-export default trpcNext.createNextApiHandler({
+// I tried but haven't found a way use edge handle without Vercel in local environment
+
+// export const config = {
+//     runtime: 'edge',
+// };
+
+const edgeHandler = async (req: NextRequest) => {
+    return fetchRequestHandler({
+        endpoint: '/api/trpc',
+        router: appRouter,
+        req,
+        createContext: () => ({}),
+    });
+};
+
+const trpcHandler = trpcNext.createNextApiHandler({
     router: appRouter,
     createContext: () => ({}),
 });
+
+export default trpcHandler;
